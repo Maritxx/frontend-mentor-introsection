6f01ee03b6e93ccb7fb7f7df1fc01e137ddd1b6e
Maritxx <maritdevries1@live.nl>
2022-07-21 19:02:01 +0200

    Changes hamburger button

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 function expandMenu () {
-let sidebarMenu = document.getElementById('sidebarMenu');
+	let sidebarMenu = document.getElementById('sidebarMenu');
 	
 	if (sidebarMenu.classList.contains('open')) {
 		sidebarMenu.classList.remove('open');
 	} else {
 		sidebarMenu.classList.add('open');
+	}
+}
+
+function expandMenu () {
+	let navIcon = document.getElementById('nav-icon');
+
+	if (navIcon.classList.contains('open')) {
+		navIcon.classList.remove('open');
+	} else {
+		navIcon.classList.add('open');
 	}
 }
