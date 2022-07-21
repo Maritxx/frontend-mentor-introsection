e01ac3fbc2590c4d08a4fc178ebb2dbb22a3ba33
Maritxx <maritdevries1@live.nl>
2022-07-21 18:31:14 +0200

    Initial set-up.

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+function expandMenu () {
+let sidebarMenu = document.getElementById('sidebarMenu');
+	
+	if (sidebarMenu.classList.contains('open')) {
+		sidebarMenu.classList.remove('open');
+	} else {
+		sidebarMenu.classList.add('open');
+	}
+}
